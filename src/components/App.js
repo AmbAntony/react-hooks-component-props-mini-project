5d736fb4c87192cdaeba45c2f2f548a220ce446a
antony waweru <anto.waweru2030@gmail.com>
2023-04-17 10:17:07 +0300

    labs

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,56 @@
 import React from "react";
 import blogData from "../data/blog";
 
-console.log(blogData);
+
 
 function App() {
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image="https://via.placeholder.com/215" about={blogData.about} />
+      <ArticleList  posts={blogData.posts}    />
+      
     </div>
+
   );
 }
 
-export default App;
+function Header({name}) {
+  return(
+  <header>
+    <h1>{name}</h1>
+  </header>
+)}
+
+function About({image, about}){
+  return(
+  <aside>
+     <img alt="blog logo" src={image}/>
+     <p>{about}</p>
+  </aside>
+)}
+
+
+function ArticleList({posts}){
+
+return(
+<main>
+  {posts.map((posts)=>(
+  <Article key={posts.id} posts={posts}  />
+))}
+</main>
+)}
+
+
+function Article({posts}){
+return(
+<article>
+  <h3>{posts.title}</h3>
+  <small>{posts.date}</small>
+  <p>{posts.preview}</p>
+</article>
+)}
+
+
+export default App; 
